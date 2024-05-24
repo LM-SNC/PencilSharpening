1f25a9e92877d7d3e072b5726d875309e72d3d9b
Apulaz <wazilinik@gmail.com>
2024-05-24 15:59:36 +0300

    init

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _timerText;
+    private DateTime _startTime;
+
+    private void Start()
+    {
+        StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        _startTime = DateTime.Now;
+        StartCoroutine(Timing());
+    }
+
+    public void StopTimer()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Timing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+            var timeSpan = DateTime.Now - _startTime;
+            _timerText.text = string.Format("{0:00}:{1:00}:{2:00}", timeSpan.Minutes, timeSpan.Seconds,
+                timeSpan.Milliseconds / 10);
+        }
+    }
+}
